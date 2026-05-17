c965492fe9cbbb5f2a5c1d8d5527534038448813
Pavlova-16-242 <pt227568@gmail.com>
2026-05-18 02:40:24 +1000

    Fix Django paths for Render

--- server/build.sh
+++ server/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+python project/manage.py collectstatic --noinput
+python project/manage.py migrate
