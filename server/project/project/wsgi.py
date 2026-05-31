58c73e6d3c35a478a590f6fc9aecae9f862dcd71
Pavlova-16-242 <pt227568@gmail.com>
2026-05-31 21:47:59 +1000

    Не говори Гоп, пока не перепрыгнешь 0.4

--- server/project/project/wsgi.py
+++ server/project/project/wsgi.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
