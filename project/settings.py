3831a262546f119f685e80d4ff33696ea3289fc4
Pavlova-16-242 <pt227568@gmail.com>
2026-06-01 01:13:36 +1000

    Не говори Гоп, пока не перепрыгнешь 0.15

--- project/settings.py
+++ project/settings.py
@@ -22,10 +22,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
     'rest_framework',
     'rest_framework.authtoken',
-    'corsheaders',
+    'corsheaders',    
+    'app',
 ]
 
 MIDDLEWARE = [
