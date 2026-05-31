42aabc8645f3ad348a957c0f0c04f2d664fc86e5
Pavlova-16-242 <pt227568@gmail.com>
2026-05-31 21:59:32 +1000

    Не говори Гоп, пока не перепрыгнешь 0.5

--- server/manage.py
+++ server/manage.py
@@ -2,7 +2,13 @@ import os
 import sys
 
 def main():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    if current_dir not in sys.path:
+        sys.path.insert(0, current_dir)
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -11,10 +17,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    
-    # Добавляем путь к проекту
-    sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
-    
     execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
