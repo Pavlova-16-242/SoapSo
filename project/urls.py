6f8c9f1753a97c6f071e9b27f52463465b799fc1
Pavlova-16-242 <pt227568@gmail.com>
2026-06-03 20:45:30 +1000

    Не говори Гоп, пока не перепрыгнешь 0.17

--- project/urls.py
+++ project/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
-    path('media/products/sea.webp', admin.site.urls),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+else:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
